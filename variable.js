c1ad41703740a4ef7e60cb6564be6c73b05599b9
Abhishek G. Hinge <124140100+softwareprofile123@users.noreply.github.com>
2023-08-28 23:45:47 +0530

    Add files via upload

---
+++ variable.js
@@ -0,0 +1,19 @@
+const accountId = 12345;
+let accountEmail = "abhi123@gmail.com";
+var accountPassword = "1234";
+accountCity = "wardha";
+let accountState;
+
+//accountId = 12;//  do not change because const variable fix 
+
+accountEmail = "ab17@gmail.com";
+accountPassword ="19";
+accountCity = "hinganghat";
+
+/*   
+ pefert do not use variable (var) because of issue in block scope and functional scope
+ 
+*/
+
+console.log("accountId");
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState ]);
