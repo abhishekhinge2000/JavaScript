ef4f256a872c1d53d8cdc49167e1ccd8363c8bf2
Abhishek G. Hinge <124140100+softwareprofile123@users.noreply.github.com>
2023-08-29 13:49:50 +0530

    Add files via upload

---
+++ conversionConfusion.js
@@ -0,0 +1,29 @@
+let score = "Abhishek";
+
+// console.log(typeof score); // string
+// console.log(typeof (score)); // string
+
+let valveInNumber = Number(score);
+
+//console.log(typeof valveInNumber); // number
+//console.log(valveInNumber); // NaN 
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1 ; false => 0 ;
+
+let isLoggedIn = "Abhishek";
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+ // console.log(booleanIsLoggedIn); // true
+
+ // 1 => true; 0 => false;
+ // " " => false;
+ // "Abhishek" => true;
+
+ let someNumber = 33;
+ let stringNumber = String( someNumber);
+ console.log(stringNumber); // 33
+ console.log(typeof stringNumber); //string
+
+
