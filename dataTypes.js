9fbe20e2e0ed59dc84fae0d4a8c183b82b662c04
Abhishek G. Hinge <124140100+softwareprofile123@users.noreply.github.com>
2023-08-29 13:49:23 +0530

    Add files via upload

---
+++ dataTypes.js
@@ -0,0 +1,26 @@
+"user Strict"; // treat all JS code as a newer version
+
+// alert( 3 + 3 ); // do not run because we are using node.js , not browser
+
+console.log( 3 + 3 ); //code readability should be high
+
+console.log("Abhishek");
+
+
+let name = "Abhishek" ;
+let age = 23 ;
+let isLoggedIn = false ;
+let state ;
+
+// number => 2 to power 53
+// bigint
+// string => " "
+// boolean => true/false
+// null => standalone valve
+// undefined =>
+// symbol => unique
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); //object
+ 
