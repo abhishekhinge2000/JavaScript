f16c0afbd0460d3b252a45afd555e07907626e02
Abhishek G. Hinge <124140100+softwareprofile123@users.noreply.github.com>
2023-08-31 16:24:38 +0530

    Add files via upload

---
+++ object3.js
@@ -0,0 +1,24 @@
+const course = {
+    coursename: "js",
+    price: "999",
+    courseInstructor: "Abhishek",
+}
+
+// course.courseInstructor
+
+const {courseInstructor: instructor} = course
+
+console.log(courseInstructor);
+//console.log(instructor);
+
+// {
+//     "name": "Abhishek",
+//     "coursename": "js ",
+//     "price": "free"
+// }
+
+[
+    {},
+    {},
+    {}
+]
