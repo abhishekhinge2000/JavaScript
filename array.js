37fab2dddb25d71268de0c2ff3bc52f250eaa877
Abhishek G. Hinge <124140100+softwareprofile123@users.noreply.github.com>
2023-08-31 12:06:26 +0530

    Add files via upload

---
+++ array.js
@@ -0,0 +1,41 @@
+//Array
+
+const myArr = [0, 1, 2, 3, 4, 5];
+const myHeors = ["shaktiman", "naagraj"];
+
+const myArr2 = new Array(1, 2, 3, 4);
+//console.log(myArr[1]);//1
+
+//-------Array Method----------//
+
+//myArr.push(6);//[0, 1, 2, 3, 4, 5, 6]
+//myArr.push(7);//[0, 1, 2, 3, 4, 5, 6, 7]
+//myArr.pop();//[0, 1, 2, 3, 4, 5, 6]
+
+//myArr.unshift(9);//[9, 0, 1, 2, 3, 4, 5]
+//myArr.shift();//[0, 1, 2, 3, 4, 5, ]
+
+//console.log(myArr.includes(9)); //false
+//console.log(myArr.indexOf(3));//3
+
+const newArr = myArr.join();
+
+//console.log(myArr); //[0, 1, 2, 3, 4, 5]
+//console.log(newArr);// 0,1,2,3,4,5
+
+//------------slice/splice------------//
+
+console.log("A", myArr);
+
+const myn1 = myArr.slice(1, 3);
+
+console.log(myn1);
+console.log("B", myArr);
+
+const myn2 = myArr.splice(1, 3);
+console.log("C",myArr);
+console.log(myn2);
+
+
+
+
